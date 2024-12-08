b94ad57b1433a7c8cec10badadad1127df114395
Roudayna17 <lsrroudayna@gmail.com>
2024-12-08 08:59:44 +0100

    'userfilter'

---
+++ client/src/app/user/userfilter/userfilter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userfilter',
+  templateUrl: './userfilter.component.html',
+  styleUrls: ['./userfilter.component.css']
+})
+export class UserfilterComponent {
+
+}
